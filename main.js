099efc95f89537c496ba62e9285ff48619817545
Koraltan Kaynak <koraltan.kaynak@deutschebahn.com>
2021-09-01 20:55:37 +0200

    update chunck size

--- main.js
+++ main.js
@@ -23,40 +23,90 @@ app.get("/", function (req, res) {
 })
 
 // Stream Video Part
+app.get('/stream/:file', (req, res) => {
 
-app.get("/video-test", function (req, res) {
-    // Ensure there is a range given for the video
+    let video_file_param = req.params.file
+    console.log("Video File : ", video_file_param)
+
+    // get video stats
+    const videoPath = VIDEO_PATH + video_file_param
+    const filePath = path.join(__dirname, videoPath)
+
+    const options = {}
+
+    let start
+    let end
+
     const range = req.headers.range
-    if (!range) {
-        res.status(400).send("Requires Range header")
+    if (range) {
+        const bytesPrefix = "bytes="
+        if (range.startsWith(bytesPrefix)) {
+            const bytesRange = range.substring(bytesPrefix.length)
+            const parts = bytesRange.split("-")
+            if (parts.length === 2) {
+                const rangeStart = parts[0] && parts[0].trim()
+                if (rangeStart && rangeStart.length > 0) {
+                    options.start = start = parseInt(rangeStart)
+                }
+                const rangeEnd = parts[1] && parts[1].trim()
+                if (rangeEnd && rangeEnd.length > 0) {
+                    options.end = end = parseInt(rangeEnd)
+                }
+            }
+        }
     }
 
-    // get video stats (about 61MB)
-    const videoPath = VIDEO_PATH + VIDEO_FILE_TEST
-    const rootPath = path.join(__dirname, videoPath)
-    const videoSize = fs.statSync(rootPath).size
+    res.setHeader("content-type", "video/mp4")
 
-    // Parse Range
-    // Example: "bytes=32324-"
-    const CHUNK_SIZE = 10 ** 6 // 1MB
-    const start = Number(range.replace(/\D/g, ""))
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+    // Open Video File
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+            console.error(`File stat error for ${filePath}.`)
+            console.error(err)
+            res.sendStatus(500)
+            return
+        }
 
-    // Create headers
-    const contentLength = end - start + 1
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    }
+        let contentLength = stat.size
 
-    // HTTP Status 206 for Partial Content
-    res.writeHead(206, headers)
+        if (req.method === "HEAD") {
+            res.statusCode = 200
+            res.setHeader("accept-ranges", "bytes")
+            res.setHeader("content-length", contentLength)
+            res.end()
+        }
+        else {        
+            let retrievedLength
+            if (start !== undefined && end !== undefined) {
+                retrievedLength = (end+1) - start
+            }
+            else if (start !== undefined) {
+                retrievedLength = contentLength - start
+            }
+            else if (end !== undefined) {
+                retrievedLength = (end+1)
+            }
+            else {
+                retrievedLength = contentLength
+            }
 
-    // create video read stream for this particular chunk
-    const videoStream = fs.createReadStream(rootPath, { start, end })
+            res.statusCode = start !== undefined || end !== undefined ? 206 : 200
 
-    // Stream the video chunk to the client
-    videoStream.pipe(res)
+            res.setHeader("content-length", retrievedLength)
+
+            if (range !== undefined) {  
+                res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`)
+                res.setHeader("accept-ranges", "bytes")
+            }
+
+            const fileStream = fs.createReadStream(filePath, options)
+            fileStream.on("error", error => {
+                console.log(`Error reading file ${filePath}.`)
+                console.log(error)
+                res.sendStatus(500)
+            })
+            
+            fileStream.pipe(res)
+        }
+    })
 })
